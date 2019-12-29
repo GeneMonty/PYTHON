bc9752753c3366e98bca47e02aab4e0c58b84d5d
GeneMonty <36083539+GeneMonty@users.noreply.github.com>
2019-12-29 02:27:06 -0600

    Spawn Drawing on mouse location

--- Turtle/myTurtle.py
+++ Turtle/myTurtle.py
@@ -1,33 +1,36 @@
 from turtle import *
 
 import turtle
+import random
 
 sc = turtle.Screen()
-foo =  turtle.Turtle()
+foo = turtle.Turtle()
 
 #render speed
-# turtle.tracer(0,0) #instant
-turtle.tracer(1,1) #normal
+turtle.tracer(0,0) #instant
+# turtle.tracer(1,1) #normal
 # turtle.tracer(3,3) #fast
 
 
 bgcolor('black')
-pendown()
+turtle.ht()
 
 clrs_1 = ['#ff0000','#ff8000','#ffc400','#ccff00','#33ff00','#00ff95']
 
 def draw(xcor,ycor):
-        foo.clear()
-        foo.reset()
-        foo.hideturtle()
-        for i in range(5):
+        # turtle.setposition(xcor,ycor)
+        turtle.reset()
+        turtle.hideturtle()
+        turtle.goto(xcor,ycor)
+        
+        for i in range(300):
+                
                 width(5)
-                rt(90)
-                fd(100+(i/100))
+                fd(10+(i*2))
+                rt(91)
                 pencolor(clrs_1[ i % 6])
-
+        
 sc.onclick(draw)
-# sc.onscreenclick(draw)
 sc.onkey(sc.bye, 'q')
 
 sc.listen()
